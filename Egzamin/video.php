f47d4d5e2f1839af523f85a86db8a0449429b9b9
Rafal223 <113582561+Rafal223@users.noreply.github.com>
2023-04-19 14:52:41 +0200

    2

--- Egzamin/video.php
+++ Egzamin/video.php
@@ -13,44 +13,83 @@
     </div>
     <div class="baner2">
         <table>
-            <td>
-                <tr>
+            <tr>
+                <td>
                     Kryminał
-                </tr>
-                <tr>
-                    20
-                </tr>
-            </td>
-            <td>
-                <tr>
+                </td>
+                <td>
                     Horror
-                </tr>
-                <tr>
-                    30
-                </tr>
-            </td>
-            <td>
-                <tr>
+                </td>
+                <td>
                     Przygodowy
-                </tr>
-                <tr>
+                </td>
+            </tr>
+            <tr>
+                <td>
                     20
-                </tr>   
-            </td>
+                </td>
+                <td>
+                    30
+                </td>
+                <td>
+                    20
+                </td>
+            </tr>
         </table>
     </div>
     <div class="lista1">
         <h3>Polecamy</h3>
-        skrypt1
+        <?php
+            $polaczenie = new mysqli("127.0.0.1","root","","dane3");
+            $sql = "SELECT id,nazwa,opis,zdjecie FROm produkty WHERE id=18 OR id=22 OR id=23 OR ID=25;";
+            $res = $polaczenie->query($sql);
+            $rows = $res->fetch_all(MYSQLI_ASSOC);
+
+            for($i=0;$i<count($rows);$i++)
+            {
+                echo "<div class='blok'><h4>".$rows[$i]["id"]." ".$rows[$i]["nazwa"]."</h4>";
+                echo "<div class='img' style='background: url(".$rows[$i]["zdjecie"].")' alt='film'></div>";
+                echo "".$rows[$i]["opis"]."</div>";
+            }
+
+            $polaczenie->close();
+        ?>
     </div>
     <div class="lista1">
         <h3>Filmy fantastyczne</h3>
-        skrypt2
+        <?php
+            $polaczenie = new mysqli("127.0.0.1","root","","dane3");
+            $sql = "SELECT id,nazwa,opis,zdjecie FROM produkty WHERE Rodzaje_id=12;";
+            $res = $polaczenie->query($sql);
+            $rows = $res->fetch_all(MYSQLI_ASSOC);
+
+            for($i=0;$i<count($rows);$i++)
+            {
+                echo "<div class='blok'><h4>".$rows[$i]["id"]." ".$rows[$i]["nazwa"]."</h4>";
+                echo "<div class='img' style='background: url(".$rows[$i]["zdjecie"].")' alt='film'></div>";
+                echo "".$rows[$i]["opis"]."</div>";
+            }
+
+            $polaczenie->close();
+        ?>
     </div>
     <div class="stopka">
         <form method="POST">
+            Usuń film nr.:<input type="Number" name="usun">
+            <input type="submit" value="Usuń film"><br>
             Stronę wykonal: <a href="mailto:ja@poczta.com">00000000000000</a>
         </form>
     </div>
+        <?php
+            $polaczenie = new mysqli("127.0.0.1","root","","dane3");
+            if(isset($_POST["usun"]))
+            {
+                $id = $_POST["usun"];
+                $sql = "DELETE FROM produkty WHERE `produkty`.`id` = ".$id.";";
+                $polaczenie->query($sql);
+            }
+
+            $polaczenie->close();
+        ?>
 </body>
 </html>
