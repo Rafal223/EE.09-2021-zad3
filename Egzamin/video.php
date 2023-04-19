4a9bd7c60832ded70b8584e3658480b28a804ada
Rafal223 <113582561+Rafal223@users.noreply.github.com>
2023-04-19 10:07:38 +0200

    1

---
+++ Egzamin/video.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <link rel="stylesheet" href="styl3.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="baner1">
+        <h1>Internetowa wypożyczalnia filmów</h1>
+    </div>
+    <div class="baner2">
+        <table>
+            <td>
+                <tr>
+                    Kryminał
+                </tr>
+                <tr>
+                    20
+                </tr>
+            </td>
+            <td>
+                <tr>
+                    Horror
+                </tr>
+                <tr>
+                    30
+                </tr>
+            </td>
+            <td>
+                <tr>
+                    Przygodowy
+                </tr>
+                <tr>
+                    20
+                </tr>   
+            </td>
+        </table>
+    </div>
+    <div class="lista1">
+        <h3>Polecamy</h3>
+        skrypt1
+    </div>
+    <div class="lista1">
+        <h3>Filmy fantastyczne</h3>
+        skrypt2
+    </div>
+    <div class="stopka">
+        <form method="POST">
+            Stronę wykonal: <a href="mailto:ja@poczta.com">00000000000000</a>
+        </form>
+    </div>
+</body>
+</html>
